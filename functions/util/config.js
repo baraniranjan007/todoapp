0f48d04da07ee1a90682e115e9da981676bafaa5
baraniranjan007 <baraniranjan00755@gmail.com>
2020-09-03 16:52:00 +0530

    removed

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports={
+  apiKey: "AIzaSyC2kk9yL58v5SHA2H7wH5imZ9LV0soXrlQ",
+  authDomain: "todoapp-b9bf4.firebaseapp.com",
+  databaseURL: "https://todoapp-b9bf4.firebaseio.com",
+  projectId: "todoapp-b9bf4",
+  storageBucket: "todoapp-b9bf4.appspot.com",
+  messagingSenderId: "134518738068",
+  appId: "1:134518738068:web:2b7ec833eb8ed63af43881",
+  measurementId: "G-TR36T11XV7"
+};
